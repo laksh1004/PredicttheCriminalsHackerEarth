15408f3a82854a0f5001cde66388b382fbdeb7e8
Laksh1004 <lakshkumar1004@yandex.com>
2018-02-21 23:59:46 +0530

    initialiser changed to RandomNormal from Uniform

--- temp.py
+++ temp.py
@@ -77,7 +77,7 @@ pred = sc.fit_transform(pred)
 ## Making the Confusion Matrix
 #from sklearn.metrics import confusion_matrix
 #cm = confusion_matrix(y_test, y_pred)
-
+#
 ## Fitting Decision Tree Classification to the Training set
 #from sklearn.tree import DecisionTreeClassifier
 #classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
@@ -93,7 +93,8 @@ pred = sc.fit_transform(pred)
 #
 ## Fitting Random Forest Classification to the Training set
 #from sklearn.ensemble import RandomForestClassifier
-#classifier = RandomForestClassifier(n_estimators = 300, criterion = 'entropy', random_state = 0)
+#classifier = RandomForestClassifier(n_estimators = 300, criterion = 'entropy', max_features = 'log2',
+#                                    random_state = 0)
 #classifier.fit(X_train, y_train)
 #
 ## Predicting the Test set results
@@ -103,7 +104,7 @@ pred = sc.fit_transform(pred)
 #from sklearn.metrics import confusion_matrix
 #cm = confusion_matrix(y_test, y_pred)
 
-#
+
 # Importing the Keras libraries and packages
 import keras
 from keras.models import Sequential
@@ -113,29 +114,29 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-classifier.add(Dense(output_dim = 50, init = 'uniform', activation = 'relu', input_dim = 70))
+classifier.add(Dense(output_dim = 50, init = 'RandomNormal', activation = 'relu', input_dim = 70))
 
 # Adding the second hidden layer
-classifier.add(Dense(output_dim = 50, init = 'uniform', activation = 'relu'))
+classifier.add(Dense(output_dim = 50, init = 'RandomNormal', activation = 'relu'))
 
 # Adding the output layer
-classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+classifier.add(Dense(output_dim = 1, init = 'RandomNormal', activation = 'sigmoid'))
 
 # Compiling the ANN
 classifier.compile(optimizer = 'Adagrad', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
-
-# Part 3 - Making the predictions and evaluating the model
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-y_pred = (y_pred > 0.5)
-
-from sklearn.preprocessing import LabelEncoder
-label = LabelEncoder()
-y_pred = label.fit_transform(y_pred)
+classifier.fit(X_train, y_train, batch_size = 8, nb_epoch = 12)
+#
+## Part 3 - Making the predictions and evaluating the model
+#
+## Predicting the Test set results
+#y_pred = classifier.predict(X_test)
+#y_pred = (y_pred > 0.5)
+#
+#from sklearn.preprocessing import LabelEncoder
+#label = LabelEncoder()
+#y_pred = label.fit_transform(y_pred)
 
 ## Fitting XGBoost to the Training set
 #from xgboost import XGBClassifier
